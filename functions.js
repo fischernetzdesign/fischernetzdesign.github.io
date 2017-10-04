ab2b7ef39e2d41928b8f0269fad3e06416776ab6
fischernetzdesign <fischer.michelle.c@gmail.com>
2017-10-04 16:18:15 +0200

    New Website

---
+++ functions.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    //Smooth scroll fuer Anker
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+});
